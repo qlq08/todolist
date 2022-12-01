bfde154a8c8edb225cc4b8c4169069f4a358bbda
qlq08 <103029755+qlq08@users.noreply.github.com>
2022-12-01 23:22:33 +0800

    完成了todolist备忘录的开发

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue'
 import App from './App.vue'
+import 'ant-design-vue/dist/antd.css'
 import store from './store'
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(Antd).use(store).mount('#app')
